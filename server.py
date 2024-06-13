f145196e6f9e1069fbd082f2aa3a75eea02f8324
Eng-antoon <47995601+Eng-antoon@users.noreply.github.com>
2024-06-13 12:52:12 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,10 +1,8 @@
 from flask import Flask, request, jsonify, send_from_directory
-from flask_cors import CORS
 from datetime import datetime
 import os
 
 app = Flask(__name__)
-CORS(app)  # Enable CORS for all routes
 
 location_updates = []
 
@@ -50,5 +48,4 @@ def serve_index():
     return send_from_directory(os.getcwd(), 'index.html')
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=5000)
