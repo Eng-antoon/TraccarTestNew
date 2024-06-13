487f5be57beba7d88074215f105e0268d064baef
Eng-antoon <47995601+Eng-antoon@users.noreply.github.com>
2024-06-13 23:54:40 +0300

    Update server.py

--- server.py
+++ server.py
@@ -38,12 +38,12 @@ def query_db(query, args=(), one=False):
 def haversine(lat1, lon1, lat2, lon2):
     R = 6371.0  # Radius of the Earth in kilometers
     lat1 = math.radians(lat1)
-    lon1 = math.radians(lon1)
+    lon1 = math.radians(lat1)
     lat2 = math.radians(lat2)
-    lon2 = math.radians(lon2)
+    lon2 = math.radians(lat2)
     
     dlat = lat2 - lat1
-    dlon = lon2 - lon1
+    dlon = lon2 - lat1
     
     a = math.sin(dlat / 2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon / 2)**2
     c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
