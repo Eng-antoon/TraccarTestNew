59bf384b949a937d06784cab017c5bae8e71385a
Eng-antoon <47995601+Eng-antoon@users.noreply.github.com>
2024-06-15 00:57:17 +0300

    Update server.py

--- server.py
+++ server.py
@@ -20,7 +20,7 @@ firebase_credentials = {
     "auth_uri": "https://accounts.google.com/o/oauth2/auth",
     "token_uri": "https://oauth2.googleapis.com/token",
     "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-7yngb%40illa-traccartest.iam.gserviceaccount.com"
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-7yngb@illa-traccartest.iam.gserviceaccount.com"
 }
 
 cred = credentials.Certificate(firebase_credentials)
@@ -30,12 +30,12 @@ db = firestore.client()
 def haversine(lat1, lon1, lat2, lon2):
     R = 6371.0  # Radius of the Earth in kilometers
     lat1 = math.radians(lat1)
-    lon1 = math.radians(lat1)
+    lon1 = math.radians(lon1)
     lat2 = math.radians(lat2)
-    lon2 = math.radians(lat2)
+    lon2 = math.radians(lon2)
     
     dlat = lat2 - lat1
-    dlon = lon2 - lat1
+    dlon = lon2 - lon1
     
     a = math.sin(dlat / 2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon / 2)**2
     c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
