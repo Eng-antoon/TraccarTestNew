7dab4b8596a09368a62ee8756b8d945daa7c6ebb
Eng-antoon <47995601+Eng-antoon@users.noreply.github.com>
2024-06-16 14:33:14 +0300

    Update server.py

--- server.py
+++ server.py
@@ -17,9 +17,9 @@ db = firestore.client()
 def haversine(lat1, lon1, lat2, lon2):
     R = 6371.0  # Radius of the Earth in kilometers
     lat1 = math.radians(lat1)
-    lon1 = math.radians(lat1)
+    lon1 = math.radians(lon1)
     lat2 = math.radians(lat2)
-    lon2 = math.radians(lat2)
+    lon2 = math.radians(lon2)
     
     dlat = lat2 - lat1
     dlon = lon2 - lon1
